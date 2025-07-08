3dd2edff77cc254a95098a4383f486677929bee5
maxkat33 <max.katilius@gmail.com>
2025-07-08 21:16:27 +1000

    Ts issue fixed

--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { formatUtcTimestampToDate, formatTimezoneString } from "../utils.ts"
+import { formatUtcTimestampToDate } from "../utils.ts"
 
 type Props = {
     now: number
