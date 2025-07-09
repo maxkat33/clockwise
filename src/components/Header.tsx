f3cec51c337f996d9d831d413bd271147e2b6ab6
maxkat33 <max.katilius@gmail.com>
2025-07-09 20:09:05 +1000

    Tagline added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <header className={`shrink-0 px-[0.4em] py-[0.2em] font-[300] tracking-wide shadow-md shadow-slate-400 text-[2rem] md:text-[3.1rem] lg:text-[3.6rem]`}>
-      <h1>CLOCK<span className="font-[500]">WISE</span>
-      </h1>
+    <header className="shrink-0 flex justify-between items-baseline pl-[0.4em] pr-[0.8em] lg:pr-[1em] py-[0.2em] font-[300] tracking-wide shadow-md shadow-slate-400 text-[2rem] md:text-[3.1rem] lg:text-[3.6rem]">
+      <h1>CLOCK<span className="font-[500]">WISE</span></h1>
+      <p className="text-[1rem] md:text-[1.5rem] lg:text-[1.8rem]">anytime, anywhere</p>
     </header>
   )
 }
