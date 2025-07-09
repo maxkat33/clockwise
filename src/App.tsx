c77b0ef64f4a02914d8da72b0e49ae314a15b264
maxkat33 <max.katilius@gmail.com>
2025-07-09 16:00:25 +1000

    Testing issue with screen height and browser on iPhones

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './components/Home'
 
 function App() {
   return (
-    <div className={`relative min-h-screen flex flex-col bg-slate-200 font-[Lato] text-slate-800 text-base sm:text-lg md:text-xl lg:text-2xl`}>
+    <div className={`relative min-h-[100dvh] flex flex-col bg-slate-200 font-[Lato] text-slate-800 text-base sm:text-lg md:text-xl lg:text-2xl`}>
       <Header />
       <Home />
       <Footer />
